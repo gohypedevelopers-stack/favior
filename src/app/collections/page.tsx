e1ba9034a2c8e08f9c1f0f5217476a5e65460734
aniketthakur-404 <anikett331@gmail.com>
2026-08-10 17:29:28 +0530

    feat: implement collection pages with filtering, sorting, and product display components

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -17,7 +17,7 @@ export default function CollectionsIndexPage() {
     <div className="min-h-screen flex flex-col bg-white">
       <AnnouncementBar />
       <Navbar />
-      <div className="pt-20 sm:pt-24 flex-1 flex flex-col">
+      <div className="pt-14 sm:pt-16 flex-1 flex flex-col">
         <CollectionPage
           initialCollection={collectionsRegistry.all}
           initialCategory="all"
