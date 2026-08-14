767a3686707ad1b0b0f5b145d1fb677755ede971
aniketthakur-404 <anikett331@gmail.com>
2026-08-14 16:52:43 +0530

    feat: initialize e-commerce platform with full stack architecture, admin dashboard, and UI component library

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.favior.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.xelectron.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
