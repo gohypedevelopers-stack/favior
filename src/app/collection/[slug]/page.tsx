734497bde062fbb215ed2483b2a63aa2fcdd2db4
aniketthakur-404 <anikett331@gmail.com>
2026-08-17 15:43:22 +0530

    feat: add dynamic route redirects for collection pages in the app directory

--- src/app/collection/[slug]/page.tsx
+++ src/app/collection/[slug]/page.tsx
@@ -2,5 +2,8 @@ import CollectionDetailPage, {
   generateMetadata,
 } from "../../collections/[slug]/page";
 
+export const dynamic = 'force-dynamic';
+
+
 export { generateMetadata };
 export default CollectionDetailPage;
